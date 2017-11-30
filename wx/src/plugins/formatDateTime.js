f2f5532e22a43b8d57e17ef3867af576f623e910
chenjiaying <601295404@qq.com>
2017-11-30 15:36:19 +0800

    修改日期转换文件

--- wx/src/plugins/formatDateTime.js
+++ wx/src/plugins/formatDateTime.js
@@ -3,22 +3,22 @@ const formatDateTime = function(value) {
   let date = new Date(value*1000);
   let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
   let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${hours}:${minutes}`;
+  return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${hours}:${minutes}`;
 }
 // mm月dd日
 const formatDate = function(value) {
   let date = new Date(value*1000);
-  return `${date.getMonth()}月${date.getDate()}日`;
+  return `${date.getMonth()+1}月${date.getDate()}日`;
 }
 // yy年mm月dd日
 const formatFullDate = function(value) {
   let date = new Date(value*1000);
-  return `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日`;
+  return `${date.getFullYear()}年${date.getMonth()+1}月${date.getDate()}日`;
 }
 // yyyy.mm.dd
 const formatDated = function(value) {
   let date = new Date(value*1000);
-  return `${date.getFullYear()}.${date.getMonth()}.${date.getDate()}`;
+  return `${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()}`;
 }
 export {
   formatDateTime,
