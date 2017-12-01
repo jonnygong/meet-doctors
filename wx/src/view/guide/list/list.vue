f5230ad2d4fa6dfc768a9b194b1953921029a6d9
chenjiaying <601295404@qq.com>
2017-12-01 10:25:44 +0800

    添加选择医导的用户列表

--- wx/src/view/guide/list/list.vue
+++ wx/src/view/guide/list/list.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="guide-list">
-    领取药膳名单
+    <div class="info" v-for="(item, index) in lists" :key="index">
+      <img :src="item.img">
+      <div class="info-content">
+        <h3>{{ item.name }}</h3>
+        <a :href="`tel:${item.tel}`">{{ item.tel }}</a>
+        <b>{{ item.address }}</b>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,8 +15,20 @@
 export default {
   data() {
     return {
-      
+      lists: []
     }
+  },
+  methods: {
+    // 获取选择该医导的用户信息
+    async apiForGetUser() {
+      const res = await this.$http.post('guideGetUser', {
+        guide_id: this.$route.params.id
+      });
+      this.lists = res.param
+    }
+  },
+  mounted() {
+    this.apiForGetUser()
   }
 }
 </script>
