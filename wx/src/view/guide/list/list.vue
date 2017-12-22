1d8e49f34432938e1011d006779d333f2524a3eb
chenjiaying <601295404@qq.com>
2017-12-22 17:05:32 +0800

    添加状态分类

--- wx/src/view/guide/list/list.vue
+++ wx/src/view/guide/list/list.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="guide-list">
-    <div class="info" v-for="(item, index) in lists" :key="index">
-      <img :src="item.img">
-      <div class="info-content">
-        <h3>{{ item.name }}</h3>
-        <a :href="`tel:${item.tel}`">{{ item.tel }}</a>
-        <b>{{ item.address }}</b>
-      </div>
-    </div>
+    <mt-navbar v-model="selected">
+      <mt-tab-item v-for="(list, index) in menus" :key="index" :id="list.id">{{ list.title }}</mt-tab-item>
+    </mt-navbar>
+
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item v-for="(list, index) in menus" :key="index" :id="list.id">
+        <div class="info" v-for="(item, i) in list.lists" :key="i">
+          <img :src="item.img">
+          <div class="info-content">
+            <h3>{{ item.name }}</h3>
+            <a :href="`tel:${item.tel}`">{{ item.tel }}</a>
+            <b>{{ item.address }}</b>
+          </div>
+        </div>
+      </mt-tab-container-item>
+    </mt-tab-container>
+
   </div>
 </template>
 
@@ -15,7 +24,11 @@
 export default {
   data() {
     return {
-      lists: []
+      selected: 1,
+      menus: [
+        { id: 1, title: '已通过', lists: [] },
+        { id: 2, title: '未通过', lists: [] }
+      ]
     }
   },
   methods: {
@@ -24,7 +37,14 @@ export default {
       const res = await this.$http.post('guideGetUser', {
         guide_id: this.$route.params.id
       });
-      this.lists = res.param
+      // this.menus.lists = res.param
+      res.param.forEach(item => {
+        if(item.status == 0) {
+          this.menus[0].lists.push(item);
+        }else {
+          this.menus[1].lists.push(item);
+        }
+      })
     }
   },
   mounted() {
