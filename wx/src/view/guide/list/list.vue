64c926667497884403d638419137b67c17c2f3fe
chenjiaying <601295404@qq.com>
2017-12-25 11:38:10 +0800

    增加上拉加载数据功能

--- wx/src/view/guide/list/list.vue
+++ wx/src/view/guide/list/list.vue
@@ -14,7 +14,13 @@
     </mt-navbar>
 
     <mt-tab-container v-model="selected">
-      <mt-tab-container-item v-for="(list, index) in menus" :key="index" :id="list.id">
+      <mt-tab-container-item 
+        v-for="(list, index) in menus" 
+        :key="index" 
+        :id="list.id" 
+        v-infinite-scroll="loadMore" 
+        infinite-scroll-disabled="loading" 
+        infinite-scroll-distance="50">
         <div class="info" v-for="(item, i) in list.lists" :key="i">
           <img :src="item.img">
           <div class="info-content">
@@ -26,6 +32,10 @@
       </mt-tab-container-item>
     </mt-tab-container>
 
+    <p v-show="loading" class="page-infinite-loading">
+      <mt-spinner type="fading-circle"></mt-spinner>
+      加载中...
+    </p>
   </div>
 </template>
 
@@ -42,10 +52,23 @@ export default {
       menus: [
         { id: 1, title: '已通过', lists: [] },
         { id: 2, title: '未通过', lists: [] }
-      ]
+      ],
+      // 上拉加载数据
+      loading: false,
+      allLoaded: false,
+      page: 1
     }
   },
   methods: {
+    // 获取今日药膳完成量，本周完成量，本月完成量
+    async apiForCount() {
+      const res = await this.$http.post('userCount', {
+        guide_id: this.$route.params.id
+      });
+      this.orders[0].num = res.param.today;
+      this.orders[1].num = res.param.tswk;
+      this.orders[2].num = res.param.tsmonth;
+    },
     // 获取选择该专家助理的用户信息
     async apiForGetUser() {
       const res = await this.$http.post('guideGetUser', {
@@ -55,19 +78,43 @@ export default {
       res.param.forEach(item => {
         if(item.status == 0) {
           this.menus[0].lists.push(item);
+          this.loading = false;
+          this.allLoaded = false;
+          this.page = 1;
         }else {
           this.menus[1].lists.push(item);
+          this.loading = false;
+          this.allLoaded = false;
+          this.page = 1;
         }
       })
     },
-    // 获取今日药膳完成量，本周完成量，本月完成量
-    async apiForCount() {
-      const res = await this.$http.post('userCount', {
-        guide_id: this.$route.params.id
-      });
-      this.orders[0].num = res.param.today;
-      this.orders[1].num = res.param.tswk;
-      this.orders[2].num = res.param.tsmonth;
+    // 上拉加载数据
+    loadMore() {
+      if(this.allLoaded) {
+        this.loading = false;
+      }else {
+        this.loading = true;
+        setTimeout(async () => {
+          this.page++;
+          const res = await this.$http.post('guideGetUser', {
+            guide_id: this.$route.params.id,
+            page: this.page
+          });
+          if(res.param.length === 0) {
+            this.allLoaded = true
+          }else {
+            res.param.forEach(item => {
+              if(item.status === 0) {
+                this.menus[0].lists.push(item);
+              }else {
+                this.menus[1].lists.push(item);
+              }
+            })
+          }
+          this.loading = false;
+        }, 1500)
+      }
     }
   },
   mounted() {
